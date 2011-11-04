8f6b986a9037ee7bf202755fe3421cad330759e1
Markus Wolf <mail@markus-wolf.de>
2011-11-04 14:46:07 +0100

    Added code

---
+++ mail+gnome-shell-calendar-open-monkey-patch@markus-wolf.de/extension.js
@@ -0,0 +1,18 @@
+/* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
+const Gio = imports.gi.Gio;
+
+function init() {
+}
+
+function enable() {
+  let calendarSettings = new Gio.Settings({ schema: 'org.gnome.desktop.default-applications.office.calendar' });
+  calendarSettings.set_string('exec', 'google-chrome https://www.google.com/calendar');
+  calendarSettings.set_boolean('needs-term', false);
+}
+
+function disable() {
+  let calendarSettings = new Gio.Settings({ schema: 'org.gnome.desktop.default-applications.office.calendar' });
+  calendarSettings.reset('exec');
+  calendarSettings.reset('needs-term');
+}
+
